865324cfe7162eae0ac6f519685d74bdfc3f323f
Thada Palata <thada.p@blockfint.com>
2020-12-20 16:46:23 +0700

    Copy dump from mysql.go

--- postgres.go
+++ postgres.go
@@ -7,6 +7,8 @@ import (
 )
 
 var (
+	// TarCmd is the path to the `tar` executable
+	TarCmd = "tar"
 	// PGDumpCmd is the path to the `pg_dump` executable
 	PGDumpCmd = "pg_dump"
 )
@@ -28,13 +30,22 @@ type Postgres struct {
 
 // Export produces a `pg_dump` of the specified database, and creates a gzip compressed tarball archive.
 func (x Postgres) Export() *ExportResult {
-	result := &ExportResult{MIME: "application/octet-stream"}
-	result.Path = fmt.Sprintf(`%v_%v.sql`, x.DB, time.Now().Unix())
-	options := append(x.dumpOptions(), "-Fp", fmt.Sprintf(`-f%v`, result.Path))
+	result := &ExportResult{MIME: "application/x-tar"}
+	dumpPath := fmt.Sprintf(`%v_%v.sql`, x.DB, time.Now().Unix())
+	options := append(x.dumpOptions(), "-Fp", fmt.Sprintf(`-r%v`, dumpPath))
 	out, err := exec.Command(PGDumpCmd, options...).Output()
 	if err != nil {
 		result.Error = makeErr(err, string(out))
+		return result
 	}
+
+	result.Path = dumpPath + ".tar.gz"
+	_, err = exec.Command(TarCmd, "-czf", result.Path, dumpPath).Output()
+	if err != nil {
+		result.Error = makeErr(err, string(out))
+		return result
+	}
+
 	return result
 }
 
